8ddc8637c364fc73c6e5b8702b8f5da3a6819f66
Keyvan Akbary <kiwwito@gmail.com>
2015-01-21 00:33:13 +0000

    Fix exports

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 module.exports = {
   Stack: require('./build/stack.js').Stack,
-  Queue: require('./build/queue.js').Queue
+  Queue: require('./build/queue.js').Queue,
+  LinkedList: require('./build/linked-list.js').LinkedList,
+  Trie: require('./build/trie.js').Trie,
+  HashMap: require('./build/hash-map.js').HashMap
 };
